a909573b3ccea9e419f0b3b863b0a901850215bd
Irina Tsarevski <irinatsarevski@icloud.com>
2022-11-27 19:22:32 +0100

    add random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,8 @@
-﻿int number1 = 3;
-int number2 = 5;
+﻿int number1 = new Random().Next(1, 10); // 1,2,3,4,5,6,7,8,9
+Console.Write(number1);
+Console.Write(" + ");
+int number2 = new Random().Next(1, 10);
+Console.Write(number2);
+Console.Write("= ");
 int result = number1 + number2;
-Console.WriteLine(result);
+Console.Write(result);
