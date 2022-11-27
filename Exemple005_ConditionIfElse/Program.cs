c08442c8ed29504494300c42a9d015a218101255
Irina Tsarevski <irinatsarevski@icloud.com>
2022-11-27 21:08:04 +0100

    add Exemple 005 Condition If Else 	# Please enter the commit message for your changes. Lines starting 		#

---
+++ Exemple005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя латиницей:  ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "irina")
+{
+    Console.WriteLine("Привет, Иринка-картинка!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
