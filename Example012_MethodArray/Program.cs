fc247816a131924a5ca2041598be642c8e5c4f95
Irina Tsarevski <irinatsarevski@icloud.com>
2022-12-03 18:14:31 +0100

    add Example 012 Method Array

---
+++ Example012_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {2, 36, 56, 71, 86, 94, 71, 79, 23, 5, 7, 71};
+
+int n = array.Length;
+int find = 71;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
